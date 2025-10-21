74674489b7513df2655d13f881d3f53b9f7af46c
Agustin <rosalesneyraagustin@gmail.com>
2025-10-21 17:12:54 -0300

    Update Cliente.java

--- src/Entidades/Cliente.java
+++ src/Entidades/Cliente.java
@@ -3,5 +3,25 @@ package Entidades;
 
 
 public class Cliente {
+    private int codCli;
+    private String nombreC;
+    private int edad;
+    private String afecciones;
+    private int dni;
+    private long telefono;
+    private boolean estado;
+    
+    public Cliente(int codCli, String nombreC, int edad, String afecciones, int dni, long telefono, boolean estado){
+        this.codCli = codCli;
+        this.nombreC = nombreC;
+        this.edad = edad;
+        this.afecciones = afecciones;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.estado = estado;
+    }
+    public Cliente(){
+    
+    }
     
 }
