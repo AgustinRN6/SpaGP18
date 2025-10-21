b7ed327e11b00e38bb3beeb316158201fd2a865a
Agustin <rosalesneyraagustin@gmail.com>
2025-10-21 15:58:34 -0300

    vista diseñador y script base de datos

---
+++ src/ScriptBD/gp18_spa_relax.sql
@@ -0,0 +1,253 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-10-2025 a las 00:53:46
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gp18_spa_relax`
+--
+CREATE DATABASE IF NOT EXISTS `gp18_spa_relax` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `gp18_spa_relax`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `codCli` int(11) NOT NULL,
+  `dni` int(10) UNSIGNED NOT NULL,
+  `nombreCompleto` varchar(100) NOT NULL,
+  `telefono` bigint(20) NOT NULL,
+  `edad` tinyint(3) UNSIGNED NOT NULL,
+  `afecciones` varchar(100) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `consultorio`
+--
+
+CREATE TABLE `consultorio` (
+  `nroConsultorio` int(11) NOT NULL,
+  `usos` int(11) NOT NULL,
+  `equipamento` varchar(100) NOT NULL,
+  `apto` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dia_de_spa`
+--
+
+CREATE TABLE `dia_de_spa` (
+  `codPack` int(11) NOT NULL,
+  `fechaHora` datetime NOT NULL,
+  `preferencias` varchar(100) NOT NULL,
+  `codCli` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  `monto` int(11) NOT NULL,
+  `cliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `instalacion`
+--
+
+CREATE TABLE `instalacion` (
+  `codInstal` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `detalleDeUso` varchar(250) NOT NULL,
+  `precio30M` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  `sesion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `masajista`
+--
+
+CREATE TABLE `masajista` (
+  `matricula` int(11) NOT NULL,
+  `nombreApellido` varchar(100) NOT NULL,
+  `telefono` bigint(20) NOT NULL,
+  `especialidad` varchar(100) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sesion`
+--
+
+CREATE TABLE `sesion` (
+  `codSesion` int(11) NOT NULL,
+  `fechaHoraInicio` datetime NOT NULL,
+  `fechaHoraFin` datetime NOT NULL,
+  `tratamiento` int(11) NOT NULL,
+  `consultorio` int(11) NOT NULL,
+  `masajista` int(11) NOT NULL,
+  `dia_De_Spa` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tratamiento`
+--
+
+CREATE TABLE `tratamiento` (
+  `codTratam` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `detalle` varchar(250) NOT NULL,
+  `duracion` time NOT NULL,
+  `costo` int(11) NOT NULL,
+  `estado` int(11) NOT NULL,
+  `tipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`codCli`);
+
+--
+-- Indices de la tabla `consultorio`
+--
+ALTER TABLE `consultorio`
+  ADD PRIMARY KEY (`nroConsultorio`);
+
+--
+-- Indices de la tabla `dia_de_spa`
+--
+ALTER TABLE `dia_de_spa`
+  ADD PRIMARY KEY (`codPack`),
+  ADD KEY `cliente` (`cliente`);
+
+--
+-- Indices de la tabla `instalacion`
+--
+ALTER TABLE `instalacion`
+  ADD PRIMARY KEY (`codInstal`),
+  ADD KEY `sesion` (`sesion`);
+
+--
+-- Indices de la tabla `masajista`
+--
+ALTER TABLE `masajista`
+  ADD PRIMARY KEY (`matricula`);
+
+--
+-- Indices de la tabla `sesion`
+--
+ALTER TABLE `sesion`
+  ADD PRIMARY KEY (`codSesion`),
+  ADD KEY `tratamiento` (`tratamiento`),
+  ADD KEY `masajista` (`masajista`),
+  ADD KEY `dia_De_Spa` (`dia_De_Spa`),
+  ADD KEY `consultorio` (`consultorio`);
+
+--
+-- Indices de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  ADD PRIMARY KEY (`codTratam`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `codCli` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `consultorio`
+--
+ALTER TABLE `consultorio`
+  MODIFY `nroConsultorio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `dia_de_spa`
+--
+ALTER TABLE `dia_de_spa`
+  MODIFY `codPack` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `instalacion`
+--
+ALTER TABLE `instalacion`
+  MODIFY `codInstal` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sesion`
+--
+ALTER TABLE `sesion`
+  MODIFY `codSesion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  MODIFY `codTratam` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `dia_de_spa`
+--
+ALTER TABLE `dia_de_spa`
+  ADD CONSTRAINT `dia_de_spa_ibfk_1` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`codCli`);
+
+--
+-- Filtros para la tabla `instalacion`
+--
+ALTER TABLE `instalacion`
+  ADD CONSTRAINT `instalacion_ibfk_1` FOREIGN KEY (`sesion`) REFERENCES `sesion` (`codSesion`);
+
+--
+-- Filtros para la tabla `sesion`
+--
+ALTER TABLE `sesion`
+  ADD CONSTRAINT `sesion_ibfk_1` FOREIGN KEY (`tratamiento`) REFERENCES `tratamiento` (`codTratam`),
+  ADD CONSTRAINT `sesion_ibfk_2` FOREIGN KEY (`masajista`) REFERENCES `masajista` (`matricula`),
+  ADD CONSTRAINT `sesion_ibfk_3` FOREIGN KEY (`dia_De_Spa`) REFERENCES `dia_de_spa` (`codPack`),
+  ADD CONSTRAINT `sesion_ibfk_4` FOREIGN KEY (`consultorio`) REFERENCES `consultorio` (`nroConsultorio`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
